e00493294bdf5b701e2edc449731e305e1a81546
SSureshC <sureshchandak1@gmail.com>
2023-12-15 20:48:03 +0530

    News app setup

---
+++ newsapp/src/main/java/com/material/newsapp/presentation/onboarding/Page.kt
@@ -0,0 +1,29 @@
+package com.material.newsapp.presentation.onboarding
+
+import androidx.annotation.DrawableRes
+import com.material.newsapp.R
+
+data class Page(
+    val title: String,
+    val description: String,
+    @DrawableRes val image: Int,
+)
+
+val pages = listOf(
+    Page(
+        title = "Lorem Ipsum is simply dummy",
+        description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        image = R.drawable.onboarding1
+    ),
+    Page(
+        title = "Lorem Ipsum is simply dummy",
+        description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        image = R.drawable.onboarding2
+    ),
+    Page(
+        title = "Lorem Ipsum is simply dummy",
+        description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        image = R.drawable.onboarding3
+    )
+)
+
---
+++ newsapp/src/main/java/com/material/newsapp/presentation/onboarding/components/OnBoardingPage.kt
@@ -0,0 +1,68 @@
+package com.material.newsapp.presentation.onboarding.components
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.material.newsapp.R
+import com.material.newsapp.presentation.Dimens
+import com.material.newsapp.presentation.onboarding.Page
+import com.material.newsapp.ui.theme.NewsAppTheme
+
+@Composable
+fun OnBoardingPage(
+    modifier: Modifier = Modifier,
+    page: Page,
+) {
+    Column(modifier = modifier) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.60f),
+            painter = painterResource(id = page.image),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(Dimens.MediumPadding1))
+        Text(
+            modifier = Modifier.padding(horizontal = Dimens.MediumPadding2),
+            text = page.title,
+            style = MaterialTheme.typography.displaySmall.copy(fontWeight = FontWeight.Bold),
+            color = colorResource(id = R.color.display_small)
+        )
+        Text(
+            modifier = Modifier.padding(horizontal = Dimens.MediumPadding2),
+            text = page.description,
+            style = MaterialTheme.typography.bodyMedium,
+            color = colorResource(id = R.color.text_medium)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun OnBoardingPagePreview() {
+    NewsAppTheme {
+        OnBoardingPage(
+            page = Page(
+                title = "Lorem Ipsum is simply dummy",
+                description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                image = R.drawable.onboarding1
+            )
+        )
+    }
+}
